865aa60d9e637fafe999f401f33e440cceca7189
dani7beth <danielledona28@gmail.com>
2021-01-13 13:57:56 -0700

    fixed login error

--- client/src/Demo/Home.js
+++ client/src/Demo/Home.js
@@ -1,11 +1,10 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 export default () => {
-  const { email } = useContext(AuthContext);
   return (
     <>
       <h1 as="h1" textAlign="center">
-        Welcome {email}
+        Welcome
       </h1>
     </>
   );
