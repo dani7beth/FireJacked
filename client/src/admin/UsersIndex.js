5af642d7d72cf592029959e88d8202ff0abf5101
Joshua Parco <jacparco@gmail.com>
2021-01-29 11:46:55 -0700

    admin and user profile pictures now update

--- client/src/admin/UsersIndex.js
+++ client/src/admin/UsersIndex.js
@@ -63,11 +63,11 @@ const UserIndex = () => {
     return users.map((user) => {
       return (
         <>
-        {/* /show_user_submissions/:user_id */}
+          {/* /show_user_submissions/:user_id */}
           <Link to={`/show_user_submissions/${user.id}/`}>  
             <h1 key={user.id}>{user.first_name} {user.last_name}</h1>
           </Link>
-            <img src={user.image} alt="blank profile"/>
+          <img src={user.image} alt="blank profile" style={{ borderRadius: "50%", width: '200px' }} />
         </>
       )
     })
