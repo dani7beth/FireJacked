91fed2a1744fd2bd069fba3994a6bae191ab60d3
Joshua Parco <jacparco@gmail.com>
2021-02-03 15:41:44 -0700

    submission show page

--- client/src/submissions/SubmissionsFormAdmin.js
+++ client/src/submissions/SubmissionsFormAdmin.js
@@ -3,16 +3,33 @@ import { useState, useEffect } from "react"
 import Axios from "axios"
 import Comments from "../components/Comments"
 import { Button, Form } from "react-bootstrap"
+import styled from 'styled-components'
 
 const SubmissionsFormAdmin = () => {
   const { submission_id } = useParams()
   const [submission, setSubmission] = useState({})
   const [checked, setChecked] = useState(false)
   const [submissionState, setSubmissionState] = useState(submission)
+  const [user, setUser] = useState({})
 
   useEffect(()=>{
     getSubmission()
   },[])
+
+  useEffect(() => {
+    if (submission) {
+      getUser()
+    }
+  }, [submission])
+
+  const getUser = async () => {
+    try {
+      let res = await Axios.get(`/api/users/${submission.user_id}`)
+      setUser(res.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const getSubmission = async () => {
     try {
@@ -50,29 +67,77 @@ const SubmissionsFormAdmin = () => {
 
   return (
     <>
-      <h1>Submission Id: {submission_id}</h1>
-      <video style={{ width: '400px', height: '300px' }} controls="true" class="embed-responsive-item">
-        <source src={submissionState.video_upload} type="video/mp4" />
-      </video>
-      <Form onSubmit={handleSubmit}>
-        <Comments submission_id = {submission_id}/>
-      </Form>
-
-      <Form onSubmit={handleSubmit}>
-        <Form.Label>Status</Form.Label>
-        <Form.Control as='select' 
-          name="status"
-          value ={submissionState.status}
-          onChange={handleChange}>
-          <option>Choose an option...</option>
-          <option>Pending</option>
-          <option>Approved</option>
-          <option >Not Approved</option>
-        </Form.Control>
-        <Button type='submit'>Submit</Button>
-      </Form>
+      <UserName>
+        <h1>{user.first_name} {user.last_name}</h1>
+      </UserName>
+      <div style={{display: "flex", justifyContent: "space-around"}}>
+        <Video>
+          <video style={{ width: '400px', height: '300px' }} controls="true" class="embed-responsive-item">
+            <source src={submissionState.video_upload} type="video/mp4" />
+          </video>
+        </Video>
+        <AdminFeedback>
+          <AdminFeedbackForm>
+            <Form onSubmit={handleSubmit}>
+              <Form.Label>Status</Form.Label>
+              <Form.Control as='select' 
+                name="status"
+                value ={submissionState.status}
+                onChange={handleChange}>
+                <option>Choose an option...</option>
+                <option>Pending</option>
+                <option>Approved</option>
+                <option >Not Approved</option>
+              </Form.Control>
+              <Button type='submit'>Submit</Button>
+            </Form>
+          </AdminFeedbackForm>
+          <AdminFeedbackForm>
+            <Comments submission_id = {submission_id}/>
+          </AdminFeedbackForm>
+        </AdminFeedback>
+      </div>
     </>
   )
 }
+
+const AdminFeedbackForm = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-evenly;
+align-self: center;
+border: 2px solid #D6D6D6;
+height: 50%;
+width: 90%;
+margin: 25px;
+border-radius: 10px;
+box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+`
+
+const UserName = styled.div`
+margin: auto;
+margin-top: 25px;
+margin-bottom 25px;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+const Video = styled.div`
+display: flex;
+align-items: flex-start;
+justify-content: center;
+align-content: center;
+flex-grow: 1
+`
+
+const AdminFeedback = styled.div`
+display: flex;
+flex-direction: column;
+align-items: start;
+justify-content: center;
+align-content: center;
+flex-grow: 1;
+`
 
 export default SubmissionsFormAdmin
