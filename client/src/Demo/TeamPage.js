6c06c3cf8a40e99fc3c998cddb0d41a21427d4a8
Landon Whitesides <whitesideslandon@gmail.com>
2021-02-04 10:36:38 -0700

    little more styling to team page

--- client/src/Demo/TeamPage.js
+++ client/src/Demo/TeamPage.js
@@ -1,17 +1,38 @@
 import styled from 'styled-components';
+import landonPhoto from "../Photos : Images/Landon_about_me_Main.jpg";
+import { GoMarkGithub } from "react-icons/go";
+import {SiLinkedin} from "react-icons/si"
+
 
 const TeamPage = () => {
   function renderTeamMember(imagelink, name, githublink, linkedinlink, description) {
     return (
-      <div style={{margin: "auto", padding: "25px", textAlign: "center", height: "600px", width: "600px"}}>
-        <img src={imagelink} alt={name} />
+      <div style={
+          {
+          display:"flex",
+          flexDirection:"column",
+          margin: "auto", 
+          padding: "25px",
+          height: "600px", 
+          width: "600px", 
+          border: "2px solid #d6d6d6",
+          borderRadius: "10px",
+          justifyContent:"center",
+          alignItems:"center",
+          marginBottom: "20px"
+          }}>
+        <img src={imagelink} alt={name} width="200px" style={{margin:"40px", borderRadius: "10px"}} />
         <h3>{name}</h3>
-        <a href={githublink}>Github Icon</a>
-        <a href={linkedinlink}>LinkedIn Icon</a>
-        <p>{description}</p>
+        <div style={{display:"flex", flexDirection: "row", margin: "5px"}}>
+          <GoMarkGithub href={githublink} fontSize="24px" style={{marginRight: "10px"}}/>
+          <SiLinkedin href={linkedinlink} fontSize="24px" style={{marginLeft: "10px"}} />
+        </div>
+        <p style={{textAlign: "center", marginTop: "5px"}}>{description}</p>
       </div>
     )
   }
+
+  let landon_description = "I'm a new developer trying to make my way in this world. I like to code, ski and play with my family. I hope to become filthy rich in my coding endevours and make whoever goes on the journey with me just as wealthy."
 
   return (
     <>
@@ -19,7 +40,7 @@ const TeamPage = () => {
         <h1>Team</h1>
       </div>
       <TeamContainer>
-        {renderTeamMember("IMAGE_LINK_HERE", "Landon Whitesides", "GITHUBLINK_HERE", "LINKEDINLINK_HERE", "DESCRIPTION_HERE")}
+        {renderTeamMember(landonPhoto, "Landon Whitesides", "https://github.com/white731", "https://www.linkedin.com/in/landon-whitesides/", landon_description)}
         {renderTeamMember("IMAGE_LINK_HERE", "NAME_HERE", "GITHUBLINK_HERE", "LINKEDINLINK_HERE", "DESCRIPTION_HERE")}
         {renderTeamMember("IMAGE_LINK_HERE", "NAME_HERE", "GITHUBLINK_HERE", "LINKEDINLINK_HERE", "DESCRIPTION_HERE")}
         {renderTeamMember("IMAGE_LINK_HERE", "NAME_HERE", "GITHUBLINK_HERE", "LINKEDINLINK_HERE", "DESCRIPTION_HERE")}
